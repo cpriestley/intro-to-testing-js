7a7811af0416cf238affa09189c6eab3602f26fe
Clayton Priestley <clayton.priestley@gmail.com>
2022-11-30 15:47:56 -0600

    add sayHello function and test cases to test.js

--- code.js
+++ code.js
@@ -2,3 +2,14 @@
 function helloWorld() {
     return "Hello, World!";
 }
+function sayHello(name) {
+    switch(toString.call(name)) {
+        case '[object Null]':
+        case '[object Undefined]':
+        case '[object Boolean]':
+            return helloWorld();
+            break;
+        default:
+    }
+    return `Hello, ${name}!`;
+}
--- tests.js
+++ tests.js
@@ -13,3 +13,34 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+describe('sayHello', function () {
+    it('should be a defined function', function () {
+        expect(typeof sayHello).toBe('function');
+    });
+    it('should return a string when called', function() {
+        expect(typeof sayHello()).toBe("string");
+    });
+    it('should return the string "Hello, Jane!" when sayHello(\'Jane\') is executed', function() {
+        expect(sayHello('Jane')).toBe("Hello, Jane!");
+    });
+    it('should return the string "Hello, Alex!" when executed with \'Alex\' as the argument', function() {
+        expect(sayHello('Alex')).toBe("Hello, Alex!");
+    });
+    it('should return the string "Hello, Pat!" when executed with \'Pat\' as the argument', function() {
+        expect(sayHello('Pat')).toBe("Hello, Pat!");
+    });
+    it('should return the string "Hello, World!" when executed without argument', function() {
+        expect(sayHello()).toBe("Hello, World!");
+    });
+    it('should return the string "Hello, World!" when executed with true as the argument', function() {
+        expect(sayHello(true)).toBe("Hello, World!");
+    });
+    it('should return the string "Hello, World!" when executed with false as the argument', function() {
+        expect(sayHello(false)).toBe("Hello, World!");
+    });
+    it('should return the string "Hello, World!" when executed with null as the argument', function() {
+        expect(sayHello(null)).toBe("Hello, World!");
+    });
+
+})
